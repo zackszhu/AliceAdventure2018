2e474b55839a53b4007ea8c00b87233789355ced
Chaojie Zhu <zackspbc@gmail.com>
2018-12-25 18:52:41 +0800

    Update View and WelcomeView to class

--- AliceAdventure/app/Editor/Scripts/EditorScripts/View.js
+++ AliceAdventure/app/Editor/Scripts/EditorScripts/View.js
@@ -1,145 +1,149 @@
-'use strict';
+const { Event } = require('./Utilities/Utilities');
 
-const {Event} = require('./Utilities/Utilities');
+class View {
+  constructor(_tag = 'Untagged', _height = -1, _width = -1, _bindElementID = null) {
+    this.tag = _tag;
+    this.height = _height;
+    this.width = _width;
+    this.bindElementID = _bindElementID;
+    this.dragData = {};
+  }
 
-// class
-var View;
+  HandleDragstart(ev, infoType, data) {
+    this.dragData = { type: infoType, data };
+    // ev.dataTransfer.setData("text/plain", JSON.stringify({type: infoType, data: data}));
+    // ev.dropEffect = 'all';
+  }
 
-// variables
-View = function(_tag = "Untagged", _height = -1, _width = -1, _bindElementID = null){
-	this.tag = _tag;
-	this.height = _height;
-	this.width = _width;
-	this.bindElementID = _bindElementID;
-};
+  HandleDragover(ev, infoType, operation) {
+    if (infoType == null || infoType === this.dragData.type) {
+      ev.preventDefault();
+      if (typeof operation === 'function') {
+        operation(this.dragData.data);
+      }
+    }
+  }
 
-// static
-var dragData = {}; // private
+  HandleDragEnter(ev, infoType, operation) {
+    if (infoType === this.dragData.type) {
+      ev.preventDefault();
+      if (typeof operation === 'function') {
+        operation(this.dragData.data);
+      }
+    }
+  }
 
-View.DragInfo = {
-	IEvent: 0, 
-	State: 1, 
-	ListedState: 2,
-	IReaction: 3, 
-	ListedIReaction: 4, 
-	GalleryImage: 5,
-	GallerySound: 6,
-	ListedObject: 7, 
-	ListedScene: 8,
-};
+  HandleDrop(ev, infoType, operation) {
+    // var info = JSON.parse(ev.dataTransfer.getData("text"));
+    if (infoType === this.dragData.type) {
+      ev.preventDefault();
+      if (typeof operation === 'function') {
+        operation(this.dragData.data);
+      }
+      this.dragData = {};
+    }
+  }
 
-View.HandleDragstart = function(ev, infoType, data){
-	dragData = {type: infoType, data: data};
-	//ev.dataTransfer.setData("text/plain", JSON.stringify({type: infoType, data: data}));
- 	ev.dropEffect = "all";
-};
+  static get Selection() {
+    return class Selection {
+      constructor() {
+        this.obj = null;
+        this.scn = null;
+        this.objOff = () => {
+          if (this.obj != null) {
+            this.obj.SelectOff();
+            this.obj = null;
+          }
+        };
+        this.scnOff = () => {
+          if (this.scn != null) {
+            this.scn.SelectOff();
+            this.scn = null;
+          }
+        };
+        this.objOn = (obj) => {
+          this.objOff();
+          if (obj != null) {
+            this.obj = obj;
+            obj.SelectOn();
+          }
+        };
+        this.scnOn = (scn) => {
+          this.scnOff();
+          if (scn != null) {
+            this.scn = scn;
+            scn.SelectOn();
+          }
+        };
+        Event.AddListener('delete-scene', (_id) => {
+          if (this.scn && this.scn.id === _id) {
+            this.objOff();
+            this.scnOff();
+            Event.Broadcast('update-selection');
+          }
+        });
+        Event.AddListener('delete-object', (_id) => {
+          if (this.obj && this.obj.id === _id) {
+            this.objOff();
+            Event.Broadcast('update-selection');
+          }
+        });
+      }
 
-View.HandleDragover = function(ev, infoType, operation){
-	if (infoType == null || infoType == dragData.type){
-		ev.preventDefault();
-		if (typeof operation == "function"){
-			operation(dragData.data);
-		}
-	}
-};
+      get object() {
+        return this.obj;
+      }
 
-View.HandleDragEnter = function(ev, infoType, operation){
-	if (infoType == dragData.type){
-		ev.preventDefault();
-		if (typeof operation == "function"){
-			operation(dragData.data);
-		}
-	}
-};
+      get scene() {
+        return this.scn;
+      }
 
-View.HandleDrop = function(ev, infoType, operation){
-	//var info = JSON.parse(ev.dataTransfer.getData("text"));
-	if (infoType == dragData.type){
-		ev.preventDefault();
-		if (typeof operation == "function"){
-			operation(dragData.data);
-		}
-		dragData = {};
-	}
+      deSelect() {
+        this.objOff();
+        this.scnOff();
+        Event.Broadcast('update-selection');
+      }
+
+      deSelectObject() {
+        this.objOff();
+        Event.Broadcast('update-selection');
+      }
+
+      selectObject(obj) {
+        this.objOn(obj);
+        if (obj.bindScene != null) this.scnOn(obj.bindScene);
+        Event.Broadcast('update-selection');
+      }
+
+      selectScene(scn) {
+        this.scnOn(scn);
+        this.objOff();
+        Event.Broadcast('update-selection');
+      }
+    };
+  }
+
+  ReloadView() {
+    // TODO
+    return;
+  }
+
+  InitView() {
+    // TODO
+    return;
+  }
 }
 
-View.Selection = (function(){ // WORKING ON: MOVE TO GLOBAL
-	var _obj = null, _scn = null;
-	var _objOff = function(){
-		if (_obj != null) {
-			_obj.SelectOff();
-			_obj = null;
-		}
-	};
-	var _scnOff = function(){
-		if (_scn != null) {
-			_scn.SelectOff();
-			_scn = null;
-		}
-	};
-	var _objOn = function(obj){
-		_objOff();
-		if (obj != null){
-			_obj = obj;
-			obj.SelectOn();
-		}
-	};
-	var _scnOn = function(scn){
-		_scnOff();
-		if (scn != null){
-			_scn = scn;
-			scn.SelectOn();
-		}
-	};
-	Event.AddListener("delete-scene", (_id)=>{
-		if (_scn && _scn.id == _id){
-			_objOff();
-			_scnOff();
-			Event.Broadcast("update-selection");
-		}
-	});
-	Event.AddListener("delete-object", (_id)=>{
-		if (_obj && _obj.id == _id){
-			_objOff();
-			Event.Broadcast("update-selection");
-		}
-	});
-	return {
-		get object() {return _obj},
-		get scene() {return _scn},
-		deSelect: function(){
-			_objOff(); 
-			_scnOff();
-			Event.Broadcast("update-selection");
-		},
-		deSelectObject: function(){
-			_objOff();
-			Event.Broadcast("update-selection");
-		},
-		selectObject: function(obj){
-			_objOn(obj);
-			if (obj.bindScene != null) _scnOn(obj.bindScene);
-			Event.Broadcast("update-selection");
-		},
-		selectScene: function(scn){
-			_scnOn(scn);
-			_objOff();
-			Event.Broadcast("update-selection");
-		},
-	};
-})();
-
-// functions
-View.prototype.InitView = function(){
-	// TODO
-	//console.log('Init view: ' + this.tag);
-	return;
-};
-
-View.prototype.ReloadView = function(){
-	// TODO
-	//console.log('Reload view: ' + this.tag);
-	return;
+View.DragInfo = {
+  IEvent: 0,
+  State: 1,
+  ListedState: 2,
+  IReaction: 3,
+  ListedIReaction: 4,
+  GalleryImage: 5,
+  GallerySound: 6,
+  ListedObject: 7,
+  ListedScene: 8,
 };
 
 module.exports = View;
