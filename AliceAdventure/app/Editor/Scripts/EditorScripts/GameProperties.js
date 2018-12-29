d7a464dde324fb29b3c4e4071511a601317fc7e4
Chaojie Zhu <zackspbc@gmail.com>
2018-12-29 16:08:52 +0800

    Refactor GameProperties

--- AliceAdventure/app/Editor/Scripts/EditorScripts/GameProperties.js
+++ AliceAdventure/app/Editor/Scripts/EditorScripts/GameProperties.js
@@ -1,318 +1,219 @@
-'use strict';
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable guard-for-in */
+let instance = null;
 
-// class
-var GameProperties;
-GameProperties = function(){
-	this.sceneList = [];
-	this.objectList = [];
-	this.interactionList = [];
-	this.stateList = [];
+class GameProperties {
+  constructor() {
+    this.sceneList = [];
+    this.objectList = [];
+    this.interactionList = [];
+    this.stateList = [];
     this.soundList = [];
     this.imageList = [];
-	this.settings = {
-		resWidth: 1280, 
-		resHeight: 960, 
-		inventoryGridNum: 5,
-		startScene: -1,
-		projectName: "untitled"
-	};
-	this.projectData = {
-		idCounter: 0,
-		viewWidth: 480, 
-		viewHeight: 360
-	};
+    this.settings = {
+      resWidth: 1280,
+      resHeight: 960,
+      inventoryGridNum: 5,
+      startScene: -1,
+      projectName: 'untitled',
+    };
+    this.projectData = {
+      idCounter: 0,
+      viewWidth: 480,
+      viewHeight: 360,
+    };
+  }
 
-	GameProperties.instance = this;
-};
-
-// singleton
-GameProperties.instance = null;
-
-// static functions
-
-//function printID(array) {
-//    var toPrint = [];
-//    for(var i in array) {
-//        toPrint.push(array[i].id)
-//    }
-//    console.log(toPrint)
-//}
-//
-//function printName(array) {
-//    var toPrint = [];
-//    for(var i in array) {
-//        toPrint.push(array[i].name)
-//    }
-//    console.log(toPrint)
-//}
-
-
-GameProperties.popSceneToTop = function(_scene) {
-    var index = -1;
-    for(var i = 0; i < GameProperties.instance.sceneList.length; i++) {
-        var scene = GameProperties.instance.sceneList[i];
-        if(scene.id == _scene.id) {
-            index = i;
-            break;
-        }
+  static get instance() {
+    if (instance === null) {
+      instance = new GameProperties();
     }
-    
+    return instance;
+  }
+
+  static getIndexOfScene(scene) {
+    for (let i = 0; i < GameProperties.instance.sceneList.length; i += 1) {
+      const currScene = GameProperties.instance.sceneList[i];
+      if (currScene.id === scene.id) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  static popSceneToTop(scene) {
+    const index = GameProperties.getIndexOfScene(scene);
+
     GameProperties.instance.sceneList.splice(index, 1);
-    GameProperties.instance.sceneList.splice(0, 0, _scene);
-    
-}
+    GameProperties.instance.sceneList.splice(0, 0, scene);
+  }
 
-GameProperties.moveSceneAfterScene = function(_sceneA, _sceneB) {
-    if(_sceneA.id == _sceneB.id) return;
-    
-    var indexA = -1;
-    var indexB = -1;
-    
-    for(var i  = 0; i < GameProperties.instance.sceneList.length; i++) {
-        var scene = GameProperties.instance.sceneList[i];
-        if(scene.id == _sceneA.id) {
-            indexA = i;
-        }
-        if(scene.id == _sceneB.id) {
-            indexB = i;
-        }
-        if(indexA!=-1 && indexB!=-1)
-            break;
-    }
+  static moveSceneAfterScene(sceneA, sceneB) {
+    const indexA = GameProperties.getIndexOfScene(sceneA);
+    const indexB = GameProperties.getIndexOfScene(sceneB);
 
-    if(indexA == indexB + 1) return;
-    
-    if(indexA < indexB) {
-        GameProperties.instance.sceneList.splice(indexB + 1, 0, _sceneA);
-        GameProperties.instance.sceneList.splice(indexA, 1);
+    if (indexA === indexB + 1) return;
+
+    if (indexA < indexB) {
+      GameProperties.instance.sceneList.splice(indexB + 1, 0, sceneA);
+      GameProperties.instance.sceneList.splice(indexA, 1);
     } else {
-        GameProperties.instance.sceneList.splice(indexA, 1);
-        GameProperties.instance.sceneList.splice(indexB + 1, 0, _sceneA);
+      GameProperties.instance.sceneList.splice(indexA, 1);
+      GameProperties.instance.sceneList.splice(indexB + 1, 0, sceneA);
     }
+  }
 
-}
-
-GameProperties.showObjNames = function(){
-	if (GameProperties.instance == null) return;
-	GameProperties.instance.objectList.forEach((obj)=>{
-		console.log(obj.name);
-	});
-}
-
-GameProperties.updateOrderByScene = function(_scene) {   
-	if (!GameProperties.ProjectLoaded()) return;
-	/*let origGood = true;
-	let origTable = "";
-    GameProperties.instance.objectList.forEach((obj, i)=>{
-    	if (obj == null){
-    		origGood = false;
-    	} else {
-    		origTable += "{" + i + ": " + obj.name + ", " + obj.bindScene.name + "}\n";
-    	}
-    });*/
-    var objInScene = _scene.container.children;
-    
-    var original = [];
-    var organized = [];
-    for(var i in GameProperties.instance.objectList) {
-        var objInList = GameProperties.instance.objectList[i];
-        if(objInList.bindScene.id == _scene.id) { 
-            for(var j in objInScene) {
-                if(objInScene[j].id == objInList.id) {
-                    organized[j] = objInList;
-                }
-            }
-            original.push(objInList);
-        }
-    }
-
-    var index = 0;
-    for(var i in GameProperties.instance.objectList) {
-        var objInList = GameProperties.instance.objectList[i];
-        if(objInList.bindScene.id == _scene.id) {
-            GameProperties.instance.objectList.splice(i,1,organized[index]);
-            index++;
-        }
-    }
-    /*let outputProblem = false;
-    let problemTable = "";
-    GameProperties.instance.objectList.forEach((obj, i)=>{
-    	if (obj == null && origGood){
-    		outputProblem = true;
-    		problemTable += "{" + i + ": null}\n"
-    	} else {
-    		problemTable += "{" + i + ": " + obj.name + ", " + obj.bindScene.name + "}\n";
-    	}
+  static showObjNames() {
+    if (GameProperties.instance === null) return;
+    GameProperties.instance.objectList.forEach((obj) => {
+      window.console.log(obj.name);
     });
-    if (outputProblem){
-    	console.log("before: \n" + origTable);
-    	console.log(objInScene);
-    	console.log(original);
-    	console.log(organized);
-    	console.log("after: \n" + problemTable);
-    }*/    
+  }
+
+  static updateOrderByScene(scene) {
+    if (!GameProperties.ProjectLoaded) return;
+    const objInScene = scene.container.children;
+
+    const original = [];
+    const organized = [];
+
+    // TODO: Refactor it later, try it out in the editor
+    for (const i in GameProperties.instance.objectList) {
+      const objInList = GameProperties.instance.objectList[i];
+      if (objInList.bindScene.id === scene.id) {
+        for (const j in objInScene) {
+          if (objInScene[j].id === objInList.id) {
+            organized[j] = objInList;
+          }
+        }
+        original.push(objInList);
+      }
+    }
+
+    let index = 0;
+    for (const i in GameProperties.instance.objectList) {
+      const objInList = GameProperties.instance.objectList[i];
+      if (objInList.bindScene.id === scene.id) {
+        GameProperties.instance.objectList.splice(i, 1, organized[index]);
+        index += 1;
+      }
+    }
+  }
+
+  static get ProjectLoaded() {
+    return instance !== null;
+  }
+
+  static SetViewSize(w, h) {
+    if (!GameProperties.ProjectLoaded) return;
+
+    GameProperties.instance.projectData.viewWidth = w;
+    GameProperties.instance.projectData.viewHeight = h;
+  }
+
+  static GetElemById(elemArray, id) {
+    if (!GameProperties.ProjectLoaded) return null;
+    return elemArray.findIndex(elem => elem.id === id);
+  }
+
+  static AddElemToArray(elem, array) {
+    if (!GameProperties.ProjectLoaded) return false;
+    array.push(elem);
+    return true;
+  }
+
+  static DeleteElemFromArray(elem, array) {
+    if (!GameProperties.ProjectLoaded) return false;
+    const index = array.indexOf(elem);
+    if (index >= 0) {
+      array.splice(elem, 1);
+      return true;
+    }
+    return false;
+  }
+
+  static GetSceneById(id) {
+    if (id === 0) {
+      return {
+        id: 0,
+        name: 'Inventory',
+      };
+    }
+    return GameProperties.GetElemById(GameProperties.instance.sceneList, id);
+  }
+
+  static GetSceneLength() {
+    if (!GameProperties.ProjectLoaded) return null;
+    return GameProperties.instance.sceneList.length;
+  }
+
+  static GetObjectById(id) {
+    return GameProperties.GetElemById(GameProperties.instance.objectList, id);
+  }
+
+  static GetInteractionById(id) {
+    return GameProperties.GetElemById(GameProperties.instance.interactionList, id);
+  }
+
+  static GetStateById(id) {
+    return GameProperties.GetElemById(GameProperties.instance.stateList, id);
+  }
+
+  static GetSoundById(id) {
+    return GameProperties.GetElemById(GameProperties.instance.soundList, id);
+  }
+
+  static GetImageById(id) {
+    return GameProperties.GetElemById(GameProperties.instance.imageList, id);
+  }
+
+  static AddScene(scene) {
+    return GameProperties.AddElemToArray(GameProperties.instance.sceneList, scene);
+  }
+
+  static DeleteScene(scene) {
+    return GameProperties.DeleteElemFromArray(GameProperties.instance.sceneList, scene);
+  }
+
+  static AddObject(obj) {
+    return GameProperties.AddElemToArray(GameProperties.instance.objectList, obj);
+  }
+
+  static DeleteObject(obj) {
+    return GameProperties.DeleteElemFromArray(GameProperties.instance.objectList, obj);
+  }
+
+  static AddInteraction(interaction) {
+    return GameProperties.AddElemToArray(GameProperties.instance.interactionList, interaction);
+  }
+
+  static DeleteInteraction(interaction) {
+    return GameProperties.DeleteElemFromArray(GameProperties.instance.interactionList, interaction);
+  }
+
+  static AddState(state) {
+    return GameProperties.AddElemToArray(GameProperties.instance.stateList, state);
+  }
+
+  static DeleteState(state) {
+    return GameProperties.DeleteElemFromArray(GameProperties.instance.stateList, state);
+  }
+
+  static AddSound(sound) {
+    return GameProperties.AddElemToArray(GameProperties.instance.soundList, sound);
+  }
+
+  static DeleteSound(sound) {
+    return GameProperties.DeleteElemFromArray(GameProperties.instance.soundList, sound);
+  }
+
+  static AddImage(image) {
+    return GameProperties.AddElemToArray(GameProperties.instance.imageList, image);
+  }
+
+  static DeleteImage(image) {
+    return GameProperties.DeleteElemFromArray(GameProperties.instance.imageList, image);
+  }
 }
-
-
-GameProperties.ProjectLoaded = function(){
-	return (GameProperties.instance != null);
-};
-
-GameProperties.SetViewSize = function(w, h){
-	if (!GameProperties.ProjectLoaded()) return null;
-	GameProperties.instance.projectData.viewWidth = w;
-	GameProperties.instance.projectData.viewHeight = h;
-};
-
-GameProperties.GetSceneById = function(_id){
-	if (!GameProperties.ProjectLoaded()) return null;
-	if (_id == 0) return {id: 0, name: 'Inventory'};
-	for (let i in GameProperties.instance.sceneList){
-		if (GameProperties.instance.sceneList[i].id == _id){
-			return GameProperties.instance.sceneList[i];
-		}
-	}
-	return null;
-};
-GameProperties.GetSceneLength = function(){
-	if (!GameProperties.ProjectLoaded()) return -1;
-	return GameProperties.instance.sceneList.length;
-};
-GameProperties.GetObjectById = function(_id){
-	if (!GameProperties.ProjectLoaded()) return null;
-	for (let i in GameProperties.instance.objectList){
-		if (GameProperties.instance.objectList[i].id == _id){
-			return GameProperties.instance.objectList[i];
-		}
-	}
-	return null;
-};
-GameProperties.GetInteractionById = function(_id){
-	if (!GameProperties.ProjectLoaded()) return null;
-	for (let i in GameProperties.instance.interactionList){
-		if (GameProperties.instance.interactionList[i].id == _id){
-			return GameProperties.instance.interactionList[i];
-		}
-	}
-	return null;
-};
-GameProperties.GetStateById = function(_id){
-	if (!GameProperties.ProjectLoaded()) return null;
-	for (let i in GameProperties.instance.stateList){
-		if (GameProperties.instance.stateList[i].id == _id){
-			return GameProperties.instance.stateList[i];
-		}
-	}
-	return null;
-};
-GameProperties.GetSoundById = function(_id){
-	if (!GameProperties.ProjectLoaded()) return null;
-	for (let i in GameProperties.instance.soundList){
-		if (GameProperties.instance.soundList[i].id == _id){
-			return GameProperties.instance.soundList[i];
-		}
-	}
-	return null;
-};
-GameProperties.GetImageById = function(_id){
-	if (!GameProperties.ProjectLoaded()) return null;
-	for (let i in GameProperties.instance.imageList){
-		if (GameProperties.instance.imageList[i].id == _id){
-			return GameProperties.instance.imageList[i];
-		}
-	}
-	return null;
-};
-
-GameProperties.AddScene = function(_scene){
-	if (!GameProperties.ProjectLoaded()) return false;
-	GameProperties.instance.sceneList.push(_scene);
-	return true;
-};
-GameProperties.DeleteScene = function(_scene){
-	if (!GameProperties.ProjectLoaded()) return false; 
-	var i = GameProperties.instance.sceneList.indexOf(_scene);
-	if (i >= 0){
-		GameProperties.instance.sceneList.splice(i, 1);
-		return true;
-	}
-	return false;
-};
-
-GameProperties.AddObject = function(_obj){
-	if (!GameProperties.ProjectLoaded()) return false;
-	GameProperties.instance.objectList.push(_obj);
-	return true;
-};
-GameProperties.DeleteObject = function(_obj){
-	if (!GameProperties.ProjectLoaded()) return false; 
-	var i = GameProperties.instance.objectList.indexOf(_obj);
-	if (i >= 0){
-		GameProperties.instance.objectList.splice(i, 1);
-		return true;
-	}
-	return false;
-};
-
-GameProperties.AddInteraction = function(_ntra){
-	if (!GameProperties.ProjectLoaded()) return false;
-	GameProperties.instance.interactionList.push(_ntra);
-	return true;
-};
-GameProperties.DeleteInteraction = function(_ntra){
-	if (!GameProperties.ProjectLoaded()) return false; 
-	var i = GameProperties.instance.interactionList.indexOf(_ntra);
-	if (i >= 0){
-		GameProperties.instance.interactionList.splice(i, 1);
-		return true;
-	}
-	return false;
-};
-
-GameProperties.AddState = function(_state){
-	if (!GameProperties.ProjectLoaded()) return false;
-	GameProperties.instance.stateList.push(_state);
-	return true;
-};
-GameProperties.DeleteState = function(_state){
-	if (!GameProperties.ProjectLoaded()) return false; 
-	var i = GameProperties.instance.stateList.indexOf(_state);
-	if (i >= 0){
-		GameProperties.instance.stateList.splice(i, 1);
-		return true;
-	}
-	return false;
-};
-
-GameProperties.AddSound = function(_sound){
-	if (!GameProperties.ProjectLoaded()) return false;
-	GameProperties.instance.soundList.push(_sound);
-	return true;
-};
-GameProperties.DeleteSound = function(_sound){
-	if (!GameProperties.ProjectLoaded()) return false; 
-	var i = GameProperties.instance.soundList.indexOf(_sound);
-	if (i >= 0){
-		GameProperties.instance.soundList.splice(i, 1);
-		return true;
-	}
-	return false;
-};
-
-GameProperties.AddImage = function(_image){
-	if (!GameProperties.ProjectLoaded()) return false;
-	// TODO detect repetitive path
-	GameProperties.instance.imageList.push(_image);
-	return true;
-};
-GameProperties.DeleteImage = function(_image){
-	if (!GameProperties.ProjectLoaded()) return false; 
-	var i = GameProperties.instance.imageList.indexOf(_image);
-	if (i >= 0){
-		GameProperties.instance.imageList.splice(i, 1);
-		return true;
-	}
-	return false;
-};
 
 module.exports = GameProperties;
