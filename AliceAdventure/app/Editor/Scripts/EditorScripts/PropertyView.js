925794d5b743caf741a329ecc1831a64ad20707d
Chaojie Zhu <zackspbc@gmail.com>
2019-01-03 15:40:39 +0800

    Update ObjectListView.js PropertyView.js SceneView.js

--- AliceAdventure/app/Editor/Scripts/EditorScripts/PropertyView.js
+++ AliceAdventure/app/Editor/Scripts/EditorScripts/PropertyView.js
@@ -1,83 +1,86 @@
-'use strict';
-
-const {Event} = require('./Utilities/Utilities');
+const {
+  Event,
+} = require('./Utilities/Utilities');
 const GameProperties = require('./GameProperties');
 const View = require('./View');
 
-// class
-var PropertyView;
+class PropertyView extends View {
+  constructor(bindElementID, height = -1, width = -1) {
+    super('PropertyView', height, width, bindElementID);
+    this.vModel = null;
+  }
 
-// variables
-PropertyView = function(_bindElementID, _height = -1, _width = -1){
-	View.call(this, "PropertyView", _height, _width, _bindElementID);
+  static NewView(elementId) {
+    return new PropertyView(elementId);
+  }
 
-	//this.bindObject = null;
-	this.vModel = null;
-	
-};
-PropertyView.prototype = new View();
+  InitView() {
+    // eslint-disable-next-line no-undef
+    this.vModel = new Vue({
+      el: `#${this.bindElementID}`,
+      data: {
+        projectLoaded: false,
+        showObject: false,
+        object: null,
+        showScene: false,
+        scene: null,
+      },
+      methods: {
+        toggleLock: () => {
+          this.vModel.object.ToggleLock();
+        },
+        deleteObject: () => {
+          this.DeleteObject();
+        },
+        deleteScene: () => {
+          this.DeleteScene();
+        },
+        // addProperty: () => {
+        //   this.bindObject.AddUserProperty(
+        //     this.vModel.propertyKey,
+        //     this.vModel.propertyType,
+        //     this.vModel.propertyValue,
+        //   );
+        // }
+      },
+    });
 
-// static
-PropertyView.NewView = function(_elementID){
-	var view = new PropertyView(_elementID);
-	view.InitView();
-	return view;
-};
+    // events
+    Event.AddListener('reload-project', () => {
+      this.ReloadView();
+    });
+    Event.AddListener('update-selection', () => {
+      this.UpdateSelection();
+    });
+  }
 
-// functions
-PropertyView.prototype.InitView = function(){
-	View.prototype.InitView.apply(this); // call super method
-	// init data binding
-	this.vModel = new Vue({
-	  el: '#' + this.bindElementID,
-	  data: {
-	  	projectLoaded: false,
-	  	showObject: false,
-	    object: null,
-	    showScene: false,
-	    scene: null
-	  }, 
-	  methods:{
-	  	toggleLock: ()=>{this.vModel.object.ToggleLock()},
-	  	deleteObject: ()=>{this.DeleteObject()},
-	  	deleteScene: ()=>{this.DeleteScene()},
-	  	//addProperty: ()=>{ this.bindObject.AddUserProperty(this.vModel.propertyKey, this.vModel.propertyType, this.vModel.propertyValue); }
-	  }
-	});
+  ReloadView() {
+    if (GameProperties.instance == null) {
+      this.vModel.projectLoaded = false;
+    } else {
+      this.vModel.projectLoaded = true;
+    }
+  }
 
-	// events
-	Event.AddListener("reload-project", ()=>{this.ReloadView();});
-	Event.AddListener("update-selection", ()=>{this.UpdateSelection();});
-};
+  UpdateSelection() {
+    this.vModel.showObject = (View.Selection.object != null);
+    this.vModel.object = View.Selection.object;
 
-PropertyView.prototype.ReloadView = function(){
-	View.prototype.ReloadView.apply(this); // call super method
+    this.vModel.showScene = (View.Selection.object == null) && (View.Selection.scene != null);
+    this.vModel.scene = View.Selection.scene;
+  }
 
-	if (GameProperties.instance == null){
-		this.vModel.projectLoaded = false;
-	} else {
-		this.vModel.projectLoaded = true;
-	}
-};
+  DeleteObject() {
+    if (confirm('Are you sure you want to delete the object?')) {
+      this.vModel.object.DeleteThis();
+    }
+  }
 
-PropertyView.prototype.UpdateSelection = function(){
-	this.vModel.showObject = (View.Selection.object != null);
-	this.vModel.object = View.Selection.object;
-
-	this.vModel.showScene = (View.Selection.object == null) && (View.Selection.scene != null);
-	this.vModel.scene = View.Selection.scene;
-};
-
-PropertyView.prototype.DeleteObject = function(){
-	if (confirm("Are you sure you want to delete the object?")){
-		this.vModel.object.DeleteThis();
-	}
-}
-
-PropertyView.prototype.DeleteScene = function(){
-	if (confirm("Are you sure you want to delete the scene?\n\nDeleting the scene will also delete every object in it.")){
-		this.vModel.scene.DeleteThis();
-	}
+  DeleteScene() {
+    if (confirm('Are you sure you want to delete the scene?\n\nDeleting the scene will also delete every object in it.')) {
+      this.vModel.scene.DeleteThis();
+    }
+  }
 }
 
 module.exports = PropertyView;
--- AliceAdventure/app/Editor/Scripts/EditorScripts/SceneView.js
+++ AliceAdventure/app/Editor/Scripts/EditorScripts/SceneView.js
@@ -1,145 +1,155 @@
-'use strict';
-
-const {PIXI, PROMPT, Event} = require('./Utilities/Utilities');
+const {
+  PIXI,
+  PROMPT,
+  Event,
+} = require('./Utilities/Utilities');
 const GameProperties = require('./GameProperties');
 const Scene = require('./Scene');
 const SceneObject = require('./SceneObject');
 const View = require('./View');
 
-// class
-var SceneView;
+class SceneView extends View {
+  constructor(bindElementID, height = -1, width = -1) {
+    super('SceneView', height, width, bindElementID);
+    this.app = null;
+    this.vModel = null;
+  }
 
-// variables
-SceneView = function(_bindElementID, _height = -1, _width = -1){
-	View.call(this, "SceneView", _height, _width, _bindElementID);
+  static NewView(elementID) {
+    const view = new SceneView(elementID);
+    return view;
+  }
 
-	this.app = null;
-	this.vModel = null;
-};
-SceneView.prototype = new View();
+  InitView() {
+    // eslint-disable-next-line no-undef
+    this.vModel = new Vue({
+      el: `#${this.bindElementID}`,
+      data: {
+        projectLoaded: false,
+      },
+      methods: {
+        addScene: () => {
+          this.AddScene();
+        },
+        assetDragover: (ev) => {
+          View.HandleDragover(ev, View.DragInfo.GalleryImage);
+        },
+        assetDrop: (ev) => {
+          View.HandleDrop(ev, View.DragInfo.GalleryImage, (data) => {
+            this.AddObject(data);
+          });
+        },
+        deleteSelected: () => {
+          SceneView.DeleteSelected();
+        },
+      },
+    });
+    // Init app
+    // TODO: Width and height magic number
+    this.app = new PIXI.Application({
+      width: 480,
+      height: 360,
+      antialiasing: true,
+      backgroundcolor: 0xFFFFFF,
+    });
+    document.getElementById('canvas-container').appendChild(this.app.view);
+    GameProperties.SetViewSize(480, 360);
 
-// static
-SceneView.NewView = function(_elementID){
-	var view = new SceneView(_elementID);
-	view.InitView();
-	return view;
-};
+    // events
+    Event.AddListener('reload-project', () => {
+      this.ReloadView();
+    });
+    Event.AddListener('add-gallery-object', (_obj) => {
+      this.AddObject(_obj);
+    });
+    Event.AddListener('object-sprite-click', (_obj) => {
+      this.SelectObject(_obj);
+    });
+  }
 
-// functions
-SceneView.prototype.InitView = function(){
-	View.prototype.InitView.apply(this); // call super method
-	// Init data binding
-	this.vModel = new Vue({
-		el: '#' + this.bindElementID,
-		data: {
-			projectLoaded: false
-		}, 
-		methods: {
-			addScene: ()=>{this.AddScene();},
-			assetDragover: (ev)=>{View.HandleDragover(ev, View.DragInfo.GalleryImage);},
-			assetDrop: (ev)=>{View.HandleDrop(ev, View.DragInfo.GalleryImage, (data)=>{this.AddObject(data);});},
-			deleteSelected: ()=>{this.DeleteSelected()},
-		}
-	});
-	// Init app
-	this.app = new PIXI.Application({
-		width: 480,
-		height: 360, 
-		antialiasing: true, 
-		backgroundcolor: 0xFFFFFF
-	});
-	document.getElementById('canvas-container').appendChild(this.app.view);
-	GameProperties.SetViewSize(480, 360);
-
-	// events
-	Event.AddListener('reload-project', ()=>{this.ReloadView();});
-	Event.AddListener('add-gallery-object', (_obj)=>{this.AddObject(_obj);});
-	Event.AddListener('object-sprite-click', (_obj)=>{this.SelectObject(_obj);});
-};
-
-SceneView.prototype.ReloadView = function(){
-	View.prototype.ReloadView.apply(this); // call super method
-	this.app.stage.removeChildren();
-	if (GameProperties.instance == null){ // no project is loaded
-		this.vModel.projectLoaded = false;
-	} else { // load current project
-		this.vModel.projectLoaded = true;
-		GameProperties.instance.sceneList.forEach((scn)=>{
-			this.app.stage.addChild(scn.container);
-			if (scn.selected){
-				View.Selection.selectScene(scn);
-			}
-		});
-		GameProperties.instance.objectList.forEach((obj)=>{
-			if (obj.bindScene == null || obj.bindScene.id == 0) return;
-			obj.bindScene.container.addChild(obj.sprite);
-			if (obj.selected){
-				View.Selection.selectObject(obj);
-			}
-		});
-	}
-};
-
-SceneView.prototype.AddObject = function(_objInfo){
-	if (View.Selection.scene == null) return;
-	var _bindScene = View.Selection.scene;
-	var _obj = SceneObject.AddObject(_objInfo, _bindScene, this.app.screen.width / 2, this.app.screen.height / 2);
-	//_bindScene.container.addChild(_obj.sprite);
-	//window.setTimeout(()=>{this.SelectObject(_obj);}, 10);
-	this.SelectObject(_obj);
-	//this.app.stage.addChild(_obj.sprite);
-};
-
-SceneView.prototype.AddScene = function(_name = null){
-	var _scene;
-	if (_name == null){
-		PROMPT({
-			title: "New scene", 
-			label: "Input scene name: ", 
-			value: "new-scene"
-		}).then((_name)=>{
-			if (_name != null) {
-				_scene = Scene.AddScene(_name);
-				this.SelectScene(_scene);
-				this.app.stage.addChild(_scene.container);
-			}
-		});
-	} else {
-		_scene = Scene.AddScene(_name);
-		this.SelectScene(_scene);
-		this.app.stage.addChild(_scene.container);
-	}
-};
-
-SceneView.prototype.SelectObject = function(_obj){
-	// Select this object
-	if (_obj.selectAllowed){
-		View.Selection.selectObject(_obj);
-	}
-};
-
-SceneView.prototype.SelectScene = function(_scn){
-	// Select this object
-	View.Selection.selectScene(_scn);
-};
-
-SceneView.prototype.DeleteObject = function(obj){
-    if(confirm("Are you sure you want to delete the object?")) 
-        obj.DeleteThis();
-};
-
-SceneView.prototype.DeleteScene = function(scn){
-    if(confirm("Are you sure you want to delete the scene?\n\nDeleting the scene will also delete every object in it.")) 
-        scn.DeleteThis();
-};
-
-SceneView.prototype.DeleteSelected = function(){
-    if (!GameProperties.ProjectLoaded()) return;
-    if (View.Selection.object != null){
-        this.DeleteObject(View.Selection.object);
-    } else if (View.Selection.scene != null){
-        this.DeleteScene(View.Selection.scene);
+  ReloadView() {
+    this.app.stage.removeChildren();
+    if (GameProperties.instance == null) {
+      this.vModel.projectLoaded = false;
+    } else {
+      this.vModel.projectLoaded = true;
+      GameProperties.instance.sceneList.forEach((scn) => {
+        this.app.stage.addChild(scn.container);
+        if (scn.selected) {
+          View.Selection.selectScene(scn);
+        }
+      });
+      GameProperties.instance.objectList.forEach((obj) => {
+        if (obj.bindScene == null || obj.bindScene.id === 0) return;
+        obj.bindScene.container.addChild(obj.sprite);
+        if (obj.selected) {
+          View.Selection.selectObject(obj);
+        }
+      });
     }
-};
+  }
+
+  AddObject(objInfo) {
+    if (View.Selection.scene == null) return;
+    const bindScene = View.Selection.scene;
+    const obj = SceneObject.AddObject(objInfo, bindScene,
+      this.app.screen.width / 2, this.app.screen.height / 2);
+    // _bindScene.container.addChild(_obj.sprite);
+    // window.setTimeout(()=>{this.SelectObject(_obj);}, 10);
+    SceneView.SelectObject(obj);
+    // this.app.stage.addChild(_obj.sprite);
+  }
+
+  AddScene(name = null) {
+    if (name === null) {
+      PROMPT({
+        title: 'New Scene',
+        label: 'Input Scene Name: ',
+        value: 'new-scene',
+      }).then((sceneName) => {
+        const scene = Scene.AddScene(sceneName);
+        SceneView.SelectScene(scene);
+        this.app.stage.addChild(scene.container);
+      });
+    } else {
+      const scene = Scene.AddScene(name);
+      SceneView.SelectScene(scene);
+      this.app.stage.addChild(scene.container);
+    }
+  }
+
+  static SelectObject(obj) {
+    if (obj.selectAllowed) {
+      View.Selection.selectObject(obj);
+    }
+  }
+
+  static SelectScene(scn) {
+    View.Selection.selectScene(scn);
+  }
+
+  static DeleteObject(obj) {
+    // eslint-disable-next-line no-restricted-globals
+    if (confirm('Are you sure you want to delete the object?')) {
+      obj.DeleteThis();
+    }
+  }
+
+  static DeleteScene(scn) {
+    // eslint-disable-next-line no-restricted-globals
+    if (confirm('Are you sure you want to delete the scene?\n\nDeleting the scene will also delete every object in it.')) {
+      scn.DeleteThis();
+    }
+  }
+
+  static DeleteSelected() {
+    if (!GameProperties.ProjectLoaded()) return;
+    if (View.Selection.object != null) {
+      SceneView.DeleteObject(View.Selection.object);
+    } else if (View.Selection.scene != null) {
+      SceneView.DeleteScene(View.Selection.scene);
+    }
+  }
+}
 
 module.exports = SceneView;
